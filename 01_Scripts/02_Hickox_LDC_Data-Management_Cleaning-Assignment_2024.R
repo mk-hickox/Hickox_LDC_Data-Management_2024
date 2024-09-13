e9809985e3012498d5ad761d57565f439066eaa2
mkhickox <mk.hickox@mail.mcgill.ca>
2024-09-13 15:21:53 -0400

    complete script with output files

--- 01_Scripts/02_Hickox_LDC_Data-Management_Cleaning-Assignment_2024.R
+++ 01_Scripts/02_Hickox_LDC_Data-Management_Cleaning-Assignment_2024.R
@@ -5,35 +5,33 @@
 #Title: Data Cleaning Assignment, LDC Data Management 2024.
 
 #Data Source: BWG Database (supplied by Dr.Diane Srivastava, UBC)
-#Dataset Used: Visits
+#Dataset Used: Visits, Bromeliads
 #Working Directory: This R Project, containing the above data
 #GitHub: https://github.com/mk-hickox/Hickox_LDC_Data-Management_2024
 
 # The Problem/Goal of Script ----------------------------------------------
-This criterion is linked to a Learning OutcomeDescription: The Problem
-(1) Does the brief written description correctly identify the chosen data cleaning task, including where in the dataset(s) the issues might arise?
+#The overall tasks are outlined below, taken directly from the assignment instructions:
+#"(1) Does the brief written description correctly identify the chosen data cleaning task, including where in the dataset(s) the issues might arise?
   
   (2) Does the description include examples of the sorts of issues that were identified in the data?
 
 # instructions ------------------------------------------------------------
-
-Dates and coordinates
-Check all dates are ISO standards (yyyy-mm-dd, ISO 8601Links to an external site.). [suggested packages: lubridate; see example code in tutorial 2]. Check these dates are in the range 1997-2024.
-For the 'visits' dataframe, change the name 'date' to 'visit_date', and add columns for day, month, and year of each visit. Place these columns immediately after the date column. Lastly, add the 'date' column from 'visits' to the 'bromeliads' dataframe, immediately after the column 'collection_date'. Does the visit_date match with the 'collection_date' in the bromeliads dataframe?
-  
-  Assign a coordinate reference system to all coordinates, and then project geographic coordinates. [suggested package: sf; see example code in tutorial 2]. Make sure these coordinates are on planet Earth, i.e. somewhere between N and S poles.
+#Quoted directly from the Dates/coordinates task of the Cleaning Assignment:
+  #1. Check all dates are ISO standards (yyyy-mm-dd, ISO 8601Links to an external site.). [suggested packages: lubridate; see example code in tutorial 2]. Check these dates are in the range 1997-2024.
+  #2. For the 'visits' dataframe, change the name 'date' to 'visit_date', and add columns for day, month, and year of each visit. Place these columns immediately after the date column. 
+  #3. Lastly, add the 'date' column from 'visits' to the 'bromeliads' dataframe, immediately after the column 'collection_date'. Does the visit_date match with the 'collection_date' in the bromeliads dataframe?
+  #4. Assign a coordinate reference system to all coordinates, and then project geographic coordinates. [suggested package: sf; see example code in tutorial 2]. Make sure these coordinates are on planet Earth, i.e. somewhere between N and S poles.
 
 # Script ------------------------------------------------------------------
 ## Load Packages
 library(tidyverse)
 library(lubridate)
 library(sf)
-library(taxize)
-library(myTAI)
 library(renv)
 library(assertr)
+library(readr)
 
-## Load Dataset
+## Load Dataframe:
 getwd() #confirm working directory is project
 
 myfiles <- list.files(path = "00_RawData/BWG_database/", pattern = "*.csv", full.names = TRUE) #loads files
@@ -49,7 +47,7 @@ list2env(
 
 # 1. Check data for class/format errors -------------------------------------------
 #Problem 1:
-    #Here, I want to check that my columns of interest (Date, lat and long) are all the correct class and format.
+    #Here, I want to check that my column of interest (Date) is in the correct class and format.
 
 view(visits) #open dataset 
 str(visits) 
@@ -72,11 +70,11 @@ visits <- #Isolating the month, day, and year and adding as new columns
   mutate(visit_year= year(visits$visit_date), #getting the years of visits
        visit_month= month(visits$visit_date), #getting the month of visits
       visit_day= day(visits$visit_date)) %>% #getting day of visit
-  relocate(visit_year,visit_month,visit_day, .after = 4) #Moving the new columns after the "visit_date" column (i.e. column 4)
+  relocate(visit_year,visit_month,visit_day, .after = visit_date) #Moving the new columns after the "visit_date" column (i.e. column 4)
 
 view(visits) #confirm that all changes look good via visual inspection!
 
-range(visits$year) #Checking the range of years.The years fall between 1997-2010, which is as expected. Data pass this check.
+range(visits$visit_year) #Checking the range of years.The years fall between 1997-2010, which is as expected (i.e. between 1997-2024). Data pass this check.
 
 #Solution 2:
   #Here, I renamed the "date" column to "visit_date" using dplyr rename. 
@@ -90,15 +88,14 @@ range(visits$year) #Checking the range of years.The years fall between 1997-2010
 
 view(bromeliads) #Opening the bromeliad dataset. Looks like "visit_id" connects the visits and bromeliads datasets
 
-
 bromeliads_output<-
   visits %>%
   select(visit_date, visit_id) %>% #selecting only the "visit_date" column (which I want to move) and the visit_id (which acts as the key)
   left_join(.,bromeliads, by= "visit_id") %>% #joining tables by "visit_id"
   relocate("visit_date", .after= "collection_date") #moving the new column after "collection_date"
 
-view(bromeliads_output) #checking output visually
-names(bromeliads_visit) #checking column names and numbers
+view(bromeliads_output) #checking output visually. Looks good.
+names(bromeliads_output) #checking column names and numbers
 
 bromeliads_output %>%
   verify(collection_date == visit_date) #Checking to see if the row values for both columns are identical.
@@ -114,10 +111,8 @@ bromeliads_output %>%
   #I added the visit_date in the correct order from the visits dataset to the bromeliads dataset.
   #I compared both columns and found that 74/76 were not identical.
   #Collection_date contains many NA and later dates than visit_date.
-  #This likely means that not all bromeliads were collection and/or that collections often took place after the initial visit
+  #This likely means that not all bromeliads were collected and/or that collections often took place after the initial visit
   #Should confirm this with dataset authors to ensure that is isn't an error
-  #The cleaned and reorganied datasets were then saved in 03_Output.
-
 
 # 4. Lat/Long Projection --------------------------------------------------
 #Problem 4:
@@ -125,9 +120,9 @@ bromeliads_output %>%
   #We also want to assign a coordinate reference system and project the coordinates 
 visits_output %>%
   select(longitude,latitude) %>%
-  range(.) #output range for longitude is: -85.433 and for latitude is: 10.983.
-          #longitude should be between -180-180, and this is the case.
-          #latitude should be between -90-90 and this is the case.
+  range() #output range for longitude is: -85.433 and for latitude is: 10.983.
+          #longitude should be between -180 to +180, and this is the case.
+          #latitude should be between -90 to +90 and this is the case.
           #lat and long.data makes sense.
 
 utm_lat_long<-
@@ -137,16 +132,16 @@ utm_lat_long<-
                crs = "+proj=longlat +datum=WGS84") %>% #assign coordinate reference system WGS84
   st_transform(., crs = "+proj=utm +zone=16 +datum=WGS84") #assigning the projection 
 
-visits_output<- visits_output %>%
+visits_output<- 
+  visits_output %>%
   mutate(utm_lat_long = utm_lat_long$geometry) #saving the transformed spatial data in the dataframe 
 view(visits_output) #checking
+
+write_csv(visits_output,"./03_Output/00_Cleaned_visits.csv") #saving the cleaned visits output file
+write_csv(bromeliads_output,"./03_Output/00_Cleaned_bromeliads.csv") #saving the cleaned bromeliads output file
 
 #Solution 4:
   #I checked the range of the latitude and longitude values, and there were all within the possible range.
   #I then assigned crs: WGS84, projected the data (zone 16), and saved the projection in the output file.
-
-#The datasets are now clean and ready for analysis!
-
-
-range(visits$latitude) #Checking the range of latitude values. Range is 10.983-10.983, 
-range(visits$longitude) #Checking the longitude range. Range is -85.433- -85.433,
+  #The datasets are now clean and ready for analysis!
+  #I saved the cleaned output files in 03_Output
